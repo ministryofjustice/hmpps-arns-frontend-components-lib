e7f3d0ba050a0dad761c9b3dbdba05b9e57abdb3
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-01-28 12:05:16 +0000

    fix

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Needed by jest for running tests in watch mode
-    // 'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
