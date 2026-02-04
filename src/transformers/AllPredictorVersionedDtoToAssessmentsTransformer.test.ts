53d23bf4a608431b512806865352e8501632b5a4
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2026-02-04 10:22:27 +0000

    Lint amendments

--- src/transformers/AllPredictorVersionedDtoToAssessmentsTransformer.test.ts
+++ src/transformers/AllPredictorVersionedDtoToAssessmentsTransformer.test.ts
@@ -86,101 +86,100 @@ function getRawInputData(): AllPredictorVersionedDto[] {
 describe('All Predictor Versioned DTO To Assessments Transformer', () => {
   it('should map raw DTO fields to Assessments type for versions 1 & 2 and sort by completedDate (newest first)', () => {
     const result = transformAllPredictorVersionedDtoToAssessments(getRawInputData())
-    expect(result).toEqual(
-      [
-        {
-          'completedDateTime': '01 January 2025 at 15:21',
-          'outputVersion': '2',
-          'allReoffendingPredictor': {
-            'name': 'All Reoffending Predictor',
-            'band': 'LOW',
-            'staticOrDynamic': 'Static',
-            'score': 1.23,
-            'completedDate': '01 January 2025',
-          },
-          'violentReoffendingPredictor': {
-            'name': 'Violent Reoffending Predictor',
-            'band': 'LOW',
-            'staticOrDynamic': 'Static',
-            'score': 1.23,
-            'completedDate': '01 January 2025',
-          },
-          'seriousViolentReoffendingPredictor': {
-            'name': 'Serious Violent Reoffending Predictor',
-            'band': 'MEDIUM',
-            'staticOrDynamic': 'Static',
-            'score': 1.23,
-            'completedDate': '01 January 2025',
-          },
-          'directContactSexualReoffendingPredictor': {
-            'name': 'Direct Contact - Sexual Reoffending Predictor',
-            'band': 'VERY HIGH',
-            'staticOrDynamic': null,
-            'score': 2.81,
-            'completedDate': '01 January 2025',
-          },
-          'indirectImageContactSexualReoffendingPredictor': {
-            'name': 'Images and Indirect Contact – Sexual Reoffending Predictor',
-            'band': 'HIGH',
-            'staticOrDynamic': null,
-            'score': 1.07,
-            'completedDate': '01 January 2025',
-          },
-          'combinedSeriousReoffendingPredictor': {
-            'name': 'Combined Serious Reoffending Predictor',
-            'band': 'HIGH',
-            'staticOrDynamic': 'Static',
-            'score': 1.23,
-            'completedDate': '01 January 2025',
-          }
+    expect(result).toEqual([
+      {
+        completedDateTime: '01 January 2025 at 15:21',
+        outputVersion: '2',
+        allReoffendingPredictor: {
+          name: 'All Reoffending Predictor',
+          band: 'LOW',
+          staticOrDynamic: 'Static',
+          score: 1.23,
+          completedDate: '01 January 2025',
         },
-        {
-          'outputVersion': '1',
-          'completedDateTime': '02 January 2024 at 18:23',
-          'ogrs3PredictorScore': {
-            'name': 'OGRS',
-            'band': 'LOW',
-            'staticOrDynamic': null,
-            'score': 5,
-            'completedDate': '02 January 2024',
-          },
-          'ovpPredictorScore': {
-            'name': 'OVP',
-            'band': 'LOW',
-            'staticOrDynamic': null,
-            'score': 7,
-            'completedDate': '02 January 2024',
-          },
-          'ogpPredictorScore': {
-            'name': 'OGP',
-            'band': 'MEDIUM',
-            'staticOrDynamic': null,
-            'score': 8,
-            'completedDate': '02 January 2024',
-          },
-          'ospdcPredictorScore': {
-            'name': 'OSP-DC',
-            'band': 'VERY HIGH',
-            'staticOrDynamic': null,
-            'score': 1.07,
-            'completedDate': '02 January 2024',
-          },
-          'ospiicPredictorScore': {
-            'name': 'OSP-IIC',
-            'band': 'HIGH',
-            'staticOrDynamic': null,
-            'score': 2.81,
-            'completedDate': '02 January 2024',
-          },
-          'rsrPredictorScore': {
-            'name': 'RSR',
-            'band': 'HIGH',
-            'staticOrDynamic': 'Dynamic',
-            'score': 50.1234,
-            'completedDate': '02 January 2024',
-          }
-        }
-      ])
+        violentReoffendingPredictor: {
+          name: 'Violent Reoffending Predictor',
+          band: 'LOW',
+          staticOrDynamic: 'Static',
+          score: 1.23,
+          completedDate: '01 January 2025',
+        },
+        seriousViolentReoffendingPredictor: {
+          name: 'Serious Violent Reoffending Predictor',
+          band: 'MEDIUM',
+          staticOrDynamic: 'Static',
+          score: 1.23,
+          completedDate: '01 January 2025',
+        },
+        directContactSexualReoffendingPredictor: {
+          name: 'Direct Contact - Sexual Reoffending Predictor',
+          band: 'VERY HIGH',
+          staticOrDynamic: null,
+          score: 2.81,
+          completedDate: '01 January 2025',
+        },
+        indirectImageContactSexualReoffendingPredictor: {
+          name: 'Images and Indirect Contact – Sexual Reoffending Predictor',
+          band: 'HIGH',
+          staticOrDynamic: null,
+          score: 1.07,
+          completedDate: '01 January 2025',
+        },
+        combinedSeriousReoffendingPredictor: {
+          name: 'Combined Serious Reoffending Predictor',
+          band: 'HIGH',
+          staticOrDynamic: 'Static',
+          score: 1.23,
+          completedDate: '01 January 2025',
+        },
+      },
+      {
+        outputVersion: '1',
+        completedDateTime: '02 January 2024 at 18:23',
+        ogrs3PredictorScore: {
+          name: 'OGRS',
+          band: 'LOW',
+          staticOrDynamic: null,
+          score: 5,
+          completedDate: '02 January 2024',
+        },
+        ovpPredictorScore: {
+          name: 'OVP',
+          band: 'LOW',
+          staticOrDynamic: null,
+          score: 7,
+          completedDate: '02 January 2024',
+        },
+        ogpPredictorScore: {
+          name: 'OGP',
+          band: 'MEDIUM',
+          staticOrDynamic: null,
+          score: 8,
+          completedDate: '02 January 2024',
+        },
+        ospdcPredictorScore: {
+          name: 'OSP-DC',
+          band: 'VERY HIGH',
+          staticOrDynamic: null,
+          score: 1.07,
+          completedDate: '02 January 2024',
+        },
+        ospiicPredictorScore: {
+          name: 'OSP-IIC',
+          band: 'HIGH',
+          staticOrDynamic: null,
+          score: 2.81,
+          completedDate: '02 January 2024',
+        },
+        rsrPredictorScore: {
+          name: 'RSR',
+          band: 'HIGH',
+          staticOrDynamic: 'Dynamic',
+          score: 50.1234,
+          completedDate: '02 January 2024',
+        },
+      },
+    ])
   })
 
   it('should error when version not 1 or 2', () => {
@@ -192,6 +191,8 @@ describe('All Predictor Versioned DTO To Assessments Transformer', () => {
         output: {},
       },
     ]
-    expect(() => transformAllPredictorVersionedDtoToAssessments(badVersionData)).toThrow('Unsupported output version: 3')
+    expect(() => transformAllPredictorVersionedDtoToAssessments(badVersionData)).toThrow(
+      'Unsupported output version: 3',
+    )
   })
 })
--- src/types/AssessmentV1.ts
+++ src/types/AssessmentV1.ts
@@ -1,7 +1,7 @@
 import { Predictor } from './Predictor'
 
 export interface AssessmentV1 {
-  outputVersion: "1"
+  outputVersion: '1'
   completedDateTime: string
   ogrs3PredictorScore?: Predictor
   ovpPredictorScore?: Predictor
