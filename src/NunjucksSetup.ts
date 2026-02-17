377bf983402d628e55bf0d45a4c334b5e003bab2
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2026-02-17 13:27:11 +0000

    ACT-429: Implement default predictor component display configuration (#37)
    * Implement default predictor component display configuration
    
    * linting amendment
    
    * Increased lib version

--- src/NunjucksSetup.ts
+++ src/NunjucksSetup.ts
@@ -1,8 +1,80 @@
 import { Environment } from 'nunjucks'
 
 export const arnsNunjucksSetup = (env: Environment) => {
-  // TODO update with required globals and filters
-  env.addGlobal('testGlobal', 'This is a test global')
+  const predictorConfig = {
+    ogrs3: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '50%', '75%', '90%', '100%'],
+    },
+    ovp: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '30%', '60%', '80%', '100%'],
+    },
+    ogp: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '34%', '67%', '85%', '100%'],
+    },
+    ospdc: {
+      showScore: false,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: false,
+    },
+    ospiic: {
+      showScore: false,
+      hasVeryHighBand: false,
+      showBandThresholdPercentages: false,
+    },
+    rsr: {
+      showScore: true,
+      hasVeryHighBand: false,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '1%', '3%', '6.9%', '25%+'],
+    },
+    allReoffendingPredictor: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '50%', '75%', '90%', '100%'],
+    },
+    violentReoffendingPredictor: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '30%', '60%', '80%', '100%'],
+    },
+    seriousViolentReoffendingPredictor: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '1%', '3%', '6.9%', '25%+'],
+    },
+    directContactSexualReoffendingPredictor: {
+      showScore: false,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: false,
+    },
+    indirectImageContactSexualReoffendingPredictor: {
+      showScore: false,
+      hasVeryHighBand: false,
+      showBandThresholdPercentages: false,
+    },
+    combinedSeriousReoffendingPredictor: {
+      showScore: true,
+      hasVeryHighBand: true,
+      showBandThresholdPercentages: true,
+      bandThresholdPercentages: ['0%', '1%', '3%', '6.9%', '25%+'],
+    },
+  }
+
+  env.addGlobal('predictorConfig', predictorConfig)
+
+  // TODO update with required filters
   env.addFilter('testFilter', (inputString: string) => {
     return inputString.toUpperCase()
   })
