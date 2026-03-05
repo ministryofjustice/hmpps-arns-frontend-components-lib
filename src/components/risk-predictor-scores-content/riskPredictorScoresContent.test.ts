abfdbada9a18f71fb28dacfa0411a3bfdae1924d
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2026-03-05 15:02:11 +0000

    ACT-453 accordion component refactor to use detailsChevron

--- src/components/risk-predictor-scores-content/riskPredictorScoresContent.test.ts
+++ src/components/risk-predictor-scores-content/riskPredictorScoresContent.test.ts
@@ -6,7 +6,7 @@ import { AssessmentV2 } from '../../types/AssessmentV2'
 
 let dom: JSDOM = null
 
-describe('predictor-scores-accordion', () => {
+describe('risk-predictor-scores-content', () => {
   beforeAll(() => {
     dom = getInitialDom()
   })
@@ -14,13 +14,13 @@ describe('predictor-scores-accordion', () => {
   describe('should test assessment source', () => {
     it('should render the correct assessment type and date', () => {
       const riskData = getRiskTestData(null)
-      const renderedHtml = getRenderedHtml(dom, 'PREDICTOR_SCORES_ACCORDION', null, riskData)
-      validatePredictorScoresAccordionSource(renderedHtml, riskData)
+      const renderedHtml = getRenderedHtml(dom, 'RISK_PREDICTOR_SCORES_CONTENT', null, riskData)
+      validatePredictorScoresSource(renderedHtml, riskData)
     })
   })
 })
 
-export const validatePredictorScoresAccordionSource = (renderedHtml: DOMWindow, riskData: RiskData) => {
+export const validatePredictorScoresSource = (renderedHtml: DOMWindow, riskData: RiskData) => {
   const latestAssessment: AssessmentV1 | AssessmentV2 = riskData.assessments?.[0]
 
   const sourceTextElement = renderedHtml.document.querySelector('.govuk-inset-text')
