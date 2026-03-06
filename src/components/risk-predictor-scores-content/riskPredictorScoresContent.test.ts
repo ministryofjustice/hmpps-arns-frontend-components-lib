d5a73420613de8176bdd2672b1ab9ce648003eec
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2026-03-06 10:53:03 +0000

    ACT-419 all reoffending predictor content

--- src/components/risk-predictor-scores-content/riskPredictorScoresContent.test.ts
+++ src/components/risk-predictor-scores-content/riskPredictorScoresContent.test.ts
@@ -29,4 +29,37 @@ export const validatePredictorScoresSource = (renderedHtml: DOMWindow, riskData:
 
   const expectedText = `Source: ${latestAssessment.assessmentType} assessment, completed on ${latestAssessment.completedDate}`
   expect(sourceTextElement.textContent.trim()).toBe(expectedText)
+
+  const scaleSelectorList = [
+    renderedHtml.document.querySelector('[data-test-id="allReoffendingPredictor-scale"]'),
+    renderedHtml.document.querySelector('[data-test-id="violentReoffendingPredictor-scale"]'),
+    renderedHtml.document.querySelector('[data-test-id="seriousViolentReoffendingPredictor-scale"]'),
+    renderedHtml.document.querySelector('[data-test-id="directContactSexualReoffendingPredictor-scale"]'),
+    renderedHtml.document.querySelector('[data-test-id="imagesAndIndirectContactSexualReoffendingPredictor-scale"]'),
+    renderedHtml.document.querySelector('[data-test-id="combinedSeriousReoffendingPredictor-scale"]'),
+  ]
+
+  scaleSelectorList.forEach(scaleSelector => {
+    expect(scaleSelector).not.toBeNull()
+  })
+
+  const staticOrDynamicExpected = [
+    renderedHtml.document.querySelector('[data-test-id="arp-staticOrDynamic"]'),
+    renderedHtml.document.querySelector('[data-test-id="vrp-staticOrDynamic"]'),
+    renderedHtml.document.querySelector('[data-test-id="csrp-staticOrDynamic"]'),
+    renderedHtml.document.querySelector('[data-test-id="svrp-staticOrDynamic"]'),
+  ]
+
+  staticOrDynamicExpected.forEach(selector => {
+    expect(selector).not.toBeNull()
+  })
+
+  const staticOrDynamicNull = [
+    renderedHtml.document.querySelector('[data-test-id="dcsrp-staticOrDynamic"]'),
+    renderedHtml.document.querySelector('[data-test-id="iicsrp-staticOrDynamic"]'),
+  ]
+
+  staticOrDynamicNull.forEach(selector => {
+    expect(selector).toBeNull()
+  })
 }
