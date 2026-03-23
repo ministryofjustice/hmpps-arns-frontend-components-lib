ce1017cd89c559072979002fba9495195ee4a47a
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-03-23 16:43:11 +0000

    ACT-448 Add script to allow details component tracking

---
+++ src/assets/js/arns-custom-app-insights.js
@@ -0,0 +1,21 @@
+// Call this function from your app insights setup script, providing the initialised appInsights object
+export function init(appInsights) {
+
+  const containingDiv = document.querySelector('[data-module="arns-custom-app-insights"]')
+
+  // Only run this on pages with the data module
+  if (!containingDiv) {
+    return
+  }
+
+  containingDiv.querySelectorAll('details').forEach((details) => {
+    details.addEventListener('toggle', (event) => {
+      const isExpanded = event.target.open
+      trackEvent(details.id, isExpanded)
+    })
+  })
+
+  function trackEvent(id, isExpanded) {
+    appInsights.trackEvent({ name: id, properties: { isExpanded: isExpanded } })
+  }
+}
