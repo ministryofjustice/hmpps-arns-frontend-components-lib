7a968585856ba969510eec12d82dfc0336cc6788
Ioannis Petridis <ioannis.petridis@hippodigital.co.uk>
2026-05-06 15:23:57 +0100

    Add missing tab for lint

--- src/components/badge-base/badgeBase.test.ts
+++ src/components/badge-base/badgeBase.test.ts
@@ -111,11 +111,11 @@ const validateBadge = (
 
   // Resolve 'shouldShowScore' logic (mirroring the .njk template)
   const config = (predictorConfig as any)[predictor]
-  const expectedShouldShowScore = hideScoreAndStaticOrDynamic === true ? false : showScore ?? config.showScore
+  const expectedShouldShowScore = hideScoreAndStaticOrDynamic === true ? false : (showScore ?? config.showScore)
 
   // Resolve 'shouldShowStaticOrDynamic' logic (mirroring the .njk template)
   const expectedShouldShowStaticOrDynamic =
-   expectedPredictorNameMappings[predictor].showStaticDynamic && !hideScoreAndStaticOrDynamic && staticOrDynamic
+    expectedPredictorNameMappings[predictor].showStaticDynamic && !hideScoreAndStaticOrDynamic && staticOrDynamic
 
   // Resolve unknown band logic (mirroring the .njk template)
   const shouldBeUnkownBand = !band || (expectedShouldShowScore === true && !score)
